bfea5f028352637bf0d01b14e5a009092005b356
Madhumitha437 <madhumithanagaraju0307@gmail.com>
2026-05-05 16:49:50 +0530

    Sum file changed

--- RFP/src/javaproject/sum.java
+++ RFP/src/javaproject/sum.java
@@ -4,7 +4,21 @@ public class sum {
 
 	public static void main(String args[])
 	{
-		Scanner c = new Scanner(System.in);
-		
-	}
+		int sum=0;
+		int invalidcount=0;
+		for(String arg:args)
+		{
+			try 
+			{
+				int n=Integer.parseInt(arg);
+				sum+=n;
+			}
+			catch(NumberFormatException e)
+			{
+			invalidcount++;
+			}
+		}
+		System.out.println("sum"+sum);
+		System.out.println("Invalid Count:"+invalidcount);
+}
 }

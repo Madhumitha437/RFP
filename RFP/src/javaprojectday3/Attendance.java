2ff041eb55a3fb0d5987105084b8141d6b2c6984
Madhumitha437 <madhumithanagaraju0307@gmail.com>
2026-05-07 14:30:25 +0530

    attendance program

---
+++ RFP/src/javaprojectday3/Attendance.java
@@ -0,0 +1,19 @@
+package javaprojectday3;
+import java.util.*;
+public class Attendance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int d=1;
+		int emp= (int)(Math.random()*10);
+		System.out.println(emp);
+		if(emp%2==0)
+		{
+			System.out.println("Present");
+		}
+		else
+			System.out.println("Absent");
+			
+	}
+
+}

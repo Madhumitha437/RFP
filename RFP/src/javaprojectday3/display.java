27372019a97a2ebe9e9257b3533f4106b188dfd9
Madhumitha437 <madhumithanagaraju0307@gmail.com>
2026-05-07 14:15:12 +0530

    Display program

---
+++ RFP/src/javaprojectday3/display.java
@@ -0,0 +1,10 @@
+package javaprojectday3;
+
+public class display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Progarm");
+	}
+
+}
